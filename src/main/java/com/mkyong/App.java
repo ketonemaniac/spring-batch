4218e48e426585447fe7c00bcb1bd9fc2cf0dc77
ketonemaniac <ketonemaniac@gmail.com>
2015-07-06 23:24:08 +0800

    test commit

--- src/main/java/com/mkyong/App.java
+++ src/main/java/com/mkyong/App.java
@@ -7,6 +7,11 @@ import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+/**
+ * The main entry class
+ * @author user
+ *
+ */
 public class App {
 	public static void main(String[] args) {
 
